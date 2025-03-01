f3c295dac5344e298f8efb3dcd4f863406feb299
sonam wangdus <wangdus.las010@gmail.com>
2025-03-01 17:54:57 +0530

    Initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-72 w-72 bg-amber-900'>hello world</div>
+    </>
+  )
+}
+
+export default App
